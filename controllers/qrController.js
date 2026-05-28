2d00dab15ce9adf0b4996706a01677b9e2d9c6da
yunus <programmerhacker001@gmail.com>
2026-05-28 19:55:16 +0300

    update

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -1,38 +1,34 @@
-import qrModels from '../models/qrModel.js'
-import QrCode from 'qrcode';
+import qrModels from "../models/qrModel.js";
+import QrCode from "qrcode";
 
 // للموقع QR انشاء
-const createQr = async (req,res) => {
-    try {
-        const {name, websiteUrl} = req.body;
-        // QR انشاء
-        const qrCode = await QrCode.toDataURL(websiteUrl);
-        // حفظ في قاعدة البيانات
-        const restaurantQr = await qrModels.create({
-            name,
-            websiteUrl,
-            qrCode
-        });
-        res.status(201).json({success:true,restaurantQr})
-    } catch (error) {
-        res.status(500).json({success:false,message:error.message});
+const createQr = async (req, res) => {
+  try {
+    const { name, websiteUrl } = req.body;
+    // QR انشاء
+    const qrCode = await QrCode.toDataURL(websiteUrl);
+    // حفظ في قاعدة البيانات
+    const restaurantQr = await qrModels.create({
+      name,
+      websiteUrl,
+      qrCode,
+    });
+    res.status(201).json({ success: true, restaurantQr });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const getQr = async (req,res) => {
+  try {
+    const restaurantQr = await qrModels.findOne();
+    if (!restaurantQr) {
+      return res.status(404).json({ message: "data not found" });
     }
-}
+    res.status(200).json({ success: true, restaurantQr });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-const getQr = async () => {
-    try {
-        const restaurantQr = await qrModels.findOne();
-        if(!restaurantQr){
-            return res.status(404).json({message:"data not found"})
-        }
-        res.status(200).json({success:true,restaurantQr})
-    } catch (error) {
-        res.status(500).json({success:false,message:error.message});
-    }
-}
-
-
-export {
-    createQr,
-    getQr
-}
+export { createQr, getQr };
